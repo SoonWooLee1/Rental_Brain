2135c63a91142e30e72f2e4b9825a1bc973f8e2f
SoonWooLee1 <lsw010420@gmail.com>
2025-12-12 15:14:44 +0900

    no message

---
+++ rental-brain/src/main/java/com/devoops/rentalbrain/customer/customersupport/query/controller/CustomersupportQueryFeedbackController.java
@@ -0,0 +1,33 @@
+package com.devoops.rentalbrain.customer.customersupport.query.controller;
+
+import com.devoops.rentalbrain.common.Pagination.PageResponseDTO;
+import com.devoops.rentalbrain.customer.customersupport.query.dto.*;
+import com.devoops.rentalbrain.customer.customersupport.query.service.CustomersupportQueryFeedbackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/feedbacks")
+public class CustomersupportQueryFeedbackController {
+
+    private final CustomersupportQueryFeedbackService queryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<PageResponseDTO<FeedbackDTO>> list(
+            @RequestParam(required = false) String title,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        FeedbackSearchDTO criteria = new FeedbackSearchDTO(page, size);
+        criteria.setTitle(title);
+
+        return ResponseEntity.ok(queryService.getFeedbackList(criteria));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FeedbackDTO> detail(@PathVariable Long id) {
+        return ResponseEntity.ok(queryService.getFeedbackDetail(id));
+    }
+}
